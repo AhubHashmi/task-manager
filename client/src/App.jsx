3d171b82a0a550ee46b80fa22b3357783854082e
Muhammad Ahub Hashmi <ahubhashmi40@gmail.com>
2026-07-18 19:23:59 +0500

    feat: integrate frontend components with backend apis

---
+++ client/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
+import './App.css';
+
+function App() {
+  const [refreshTrigger, setRefreshTrigger] = useState(0);
+  const [editingTask, setEditingTask] = useState(null);
+
+  const handleTaskSaved = () => {
+    setRefreshTrigger((prev) => prev + 1);
+    setEditingTask(null);
+  };
+
+  const handleEdit = (task) => {
+    setEditingTask(task);
+  };
+
+  const handleCancelEdit = () => {
+    setEditingTask(null);
+  };
+
+  return (
+    <div className="app-container">
+      <h1>Task Manager</h1>
+      <TaskForm
+        onTaskCreated={handleTaskSaved}
+        editingTask={editingTask}
+        onCancelEdit={handleCancelEdit}
+      />
+      <TaskList refreshTrigger={refreshTrigger} onEdit={handleEdit} />
+    </div>
+  );
+}
+
+export default App;
